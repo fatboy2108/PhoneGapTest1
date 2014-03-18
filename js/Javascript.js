aed0d2b5f2cd8b4425be9cc67807ae7c9700a461
fatboy2108 <robin.flannery1@hotmail.co.uk>
2014-03-18 17:14:49 +0000

    test2
    test2 added

--- js/Javascript.js
+++ js/Javascript.js
@@ -13,7 +13,7 @@ $( "#test1Button" ).click(function() {
   
 	TestNumber = 1234567;
 	
-	for (var i=1; i<10000000; i++){
+	for (var i=1; i<1000000; i++){
 	
 		TestNumber = TestNumber / i;
 		TestNumber = TestNumber * 50;
@@ -31,6 +31,34 @@ $( "#test1Button" ).click(function() {
  });
  
  
+ 
+ 
+ 
+$( "#test2Button" ).click(function() {
+
+	$( "#test2Results" ).html("");
+	var value1 = 0;
+	var value2 = 0;
+	var TimeTaken = 0;
+			
+	value1 = $.now();
+	$( "#test2Results" ).append ("Time at start of test2: " + value1 + "</br>");
+  
+	
+	
+	for (var i=1; i<1000; i++){
+	
+		$( "#test2Results" ).append(i);
+		
+	};
+	
+	value2 = $.now();
+	$( "#test2Results" ).append ("</br>" + "Time at end of test2: " + value2 + "</br>");
+	
+	TimeTaken = value2-value1;
+	$( "#test2Results" ).append ("The test took: : " + TimeTaken + " Millisecounds</br>");
+	
+ });
  
  
  
